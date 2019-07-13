11d1169919ae5b3f6be75500881bc8fc1d028063
Dalma Lipcsei <lipcsei.dalma@gmail.com>
2019-07-13 22:19:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function happyHolidays(string) {
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name)
-  return
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+name = 'you';
 
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
