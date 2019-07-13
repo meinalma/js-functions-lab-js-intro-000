29601cb59ab6d405058531e0ea409a2729f4ed16
Dalma Lipcsei <lipcsei.dalma@gmail.com>
2019-07-13 22:34:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name) {
 }
 happyHolidaysTo("you");
 
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
